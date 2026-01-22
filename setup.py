15138b5ef0f8fb2d2102260efd1183ade77d4594
Heather Robinson <126197658+robinsonha@users.noreply.github.com>
2026-01-22 05:32:36 +0000

    Update and rename setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ckni-search",
+    version="0.1.0",
+    description="Search CNKI for English drugs translated to Chinese across major pathways",
+    author="Your Name",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "googletrans==4.0.0-rc1",
+        "pandas",
+        "MagicCNKI"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ckni-search=ckni_search.main:main"
+        ]
+    },
+    python_requires=">=3.8"
+)
